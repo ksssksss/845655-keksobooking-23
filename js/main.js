8448b17b9679b2619f154b7fe3c54e7a8b70161a
Kseniya <aleksandrova.ksu@yandex.ru>
2021-07-11 20:57:38 +0300

    Добавление интерактивной карты

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
 import {getAds} from './data.js';
 import {initAdFilters} from './form.js';
-import {getCard, renderCard} from './card.js';
+import {initMap} from './map.js';
 
-const mapCanvas = document.querySelector('#map-canvas');
 const dataForCards = getAds();
 
 initAdFilters();
-renderCard(mapCanvas, getCard(dataForCards[0]));
-initAdFilters();
+initMap(dataForCards);
